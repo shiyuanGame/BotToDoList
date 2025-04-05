1c3a891c1ef9b817ce2848da6176124e8743c28b
shiyuanGame <shiyuanGames@gmail.com>
2025-04-06 01:32:57 +0800

    更新

--- main.py
+++ main.py
@@ -139,17 +139,17 @@ class MyPlugin(BasePlugin):
             parsed_time =tittle[1]
             if parsed_time:
                 # 注册任务
-                ctx.add_return("reply", [f"✅ 已为你设置提醒：{title}\n📅 时间：{parsed_time.strftime('%Y-%m-%d %H:%M:%S')}"])
+                # ctx.add_return("reply", [f"✅ 已为你设置提醒：{title}\n📅 时间：{parsed_time.strftime('%Y-%m-%d %H:%M:%S')}"])
                 # 封装带参数的匿名任务 
                 scheduler.add_job( 
-                    lambda: (  
-                                                ctx.add_return("reply", [f"✅ 正在对您进行提醒：{title}\n📅  "])
+                    lambda: asyncio.create_task(  
+                                                ctx.add_return("reply", [f"✅ 正在对您进行提醒：{title}，，，{scheduler.get_jjobs()}\n📅  "])
                                                 
                                                 ),
                     'date',
                     run_date=parsed_time.strftime('%Y-%m-%d %H:%M:%S'), 
                 )
-                ctx.add_return("reply", [f"✅ 已为你设置提醒列表：{scheduler.get_jjobs()} "])
+                # ctx.add_return("reply", [f"✅ 已为你设置提醒列表：{scheduler.get_jjobs()} "])
                 ctx.prevent_default()
                 return
         except Exception as e:
